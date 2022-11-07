031833785f546c763bda2505a4eebebe8b2dc2a7
MatheusStefan <roccomatheus0@gmail.com>
2022-11-07 09:28:10 -0300

    Style component

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -42,7 +42,49 @@ class CardNews extends HTMLElement {
     }
 
     styles() {
+        const style = document.createElement("style");
+        style.textContent = `
+            .card {
+                width: 100%;
+                display: flex;
+                -webkit-box-shadow: 38px 23px 13px -10px rgba(0,0,0,0.75);
+                -moz-box-shadow: 38px 23px 13px -10px rgba(0,0,0,0.75);
+                box-shadow: 38px 23px 13px -10px rgba(0,0,0,0.75);
+                flex-direction: row;
+                justify-content: space-between;
+            }
+            
+            .card__left {
+                display: flex;
+                flex-direction: column;
+                justify-content:center;
+                padding: 0px 10px;
+            }
+            
+            .card__left > span {
+                font-style: italic;
+                font-weight: bold;
+            }
+            
+            .card__left > h1 {
+                margin-top: 15px;
+                margin-bottom: 2px;
+                font-size: 2.1rem;
+            }
+            
+            .card__left > p {
+                color: rgb(99, 88, 88);
+            }
 
+            .card__right img {
+                max-width: 100%;
+                max-height: 100%;
+                width: 300px;
+                height: 177px;
+            }
+        `;
+
+        return style;
     }
 }
 
