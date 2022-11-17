4669b4fb9822c3592f39cbfb1640d6f10f5262b4
MatheusStefan <roccomatheus0@gmail.com>
2022-11-17 14:45:08 -0300

    Momentos de ciclo de vida de um componente

--- life-cycle/src/app/title/title.component.ts
+++ life-cycle/src/app/title/title.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() nome: string = ''
 
-  constructor() { }
+  constructor() {
+    console.log(`Construtor ${this.nome}`)
+  }
+  ngOnChanges(): void {
+    console.log(`OnChange ${this.nome}`);
+  }
 
   ngOnInit(): void {
+    console.log(`OnInit ${this.nome}`)
   }
+
 
 }
