07f9fded288f1a29acb68f93b49bddba8e94ee36
MatheusStefan <roccomatheus0@gmail.com>
2022-12-05 13:32:01 -0300

    QueryParams

--- rotas/rotas/src/app/pages/portfolio/card/card.component.ts
+++ rotas/rotas/src/app/pages/portfolio/card/card.component.ts
@@ -12,6 +12,9 @@ export class CardComponent implements OnInit {
     this.activeRoute.params.subscribe(
       res => console.log(res)
     )
+    this.activeRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
   }
 
   ngOnInit(): void {
