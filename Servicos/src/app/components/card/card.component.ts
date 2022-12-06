db0b4ef27fa6d659732217bb1e764384659830a8
MatheusStefan <roccomatheus0@gmail.com>
2022-12-06 16:20:35 -0300

    Tipando os dados e buscando dinamicamente

--- Servicos/src/app/components/card/card.component.ts
+++ Servicos/src/app/components/card/card.component.ts
@@ -8,16 +8,25 @@ import { PokemonService } from 'src/app/services/pokemon.service';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  pokemon: PokemonData | any
+  pokemon: PokemonData
   name: string = "Squirtle"
   types: string[] = ['Water']
 
   constructor(
     private service: PokemonService
-  ) { }
+  ) {
+    this.pokemon = {
+      id: 0,
+      name: '',
+      sprites: {
+        front_default: ''
+      },
+    types:[]
+  }
+}
 
   ngOnInit(): void {
-    this.service.getPokemon("charizard").subscribe(
+    this.service.getPokemon("ivysaur").subscribe(
       {
         next: (res) => {
 
@@ -27,10 +36,6 @@ export class CardComponent implements OnInit {
             sprites: res.sprites,
             types: res.types
           }
-
-          console.log(res)
-          console.log(this.pokemon)
-
         },
         error: (err) => console.log('not found')
       }
